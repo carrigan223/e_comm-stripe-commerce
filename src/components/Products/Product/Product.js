5de3caf6009aaec43bfa90e7eb2a39b527a9cae3
Andrew Carrigan <carrigan223@gmail.com>
2021-03-07 23:31:45 -0800

    added Navbar

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -8,9 +8,37 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
 
-const Product = () => {
-  return <div></div>;
+const Product = ({ product }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardMedia
+          img={product.image}
+          title={product.name}
+          className={classes.media}
+        />
+        <CardContent>
+          <div className={classes.cardContent}>
+            <Typography variant="h5" gutterBottom>
+              {product.name}
+            </Typography>
+            <Typography variant="h5">{product.price}</Typography>
+          </div>
+          <Typography variant="body2" color="textSecondary">
+            {product.description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+          <IconButton aria-label="Add to Cart">
+            <AddShoppingCart />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,43 @@
 import React from "react";
-import Grid from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+
+import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running Shoes" },
-  { id: 2, name: "Macbook", description: "Apple Laptop" },
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running Shoes",
+    price: 1.5,
+    image:
+      "https://images.unsplash.com/photo-1541876176131-3f5e84a7331a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description: "Apple Laptop",
+    price: 1.05,
+    image:
+      "https://images.unsplash.com/photo-1541876176131-3f5e84a7331a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
+  },
 ];
 
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product />
-        </Grid>
-      ))}
-    </Grid>
-  </main>;
+  const classes = useStyles();
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 };
 
 export default Products;
